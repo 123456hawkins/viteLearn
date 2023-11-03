84bd652ec4a8d4d5008139d1f296d45b4e021d39
123456hawkins <2398430768@qq.com>
2023-11-03 15:20:00 +0800

    新增环境变量

--- tutorial/vite.config.js
+++ tutorial/vite.config.js
@@ -1,24 +1,32 @@
 import { defineConfig } from 'vite' //用了这个就有语法提示
+import { loadEnv } from 'vite'
 import viteBaseConfig from './vite.base.config'
 import viteDevConfig from './vite.dev.config'
 import viteProdConfig from './vite.prod.config'
+
 // 策略模式
 const envResolver = {
   // build: () => Object.assign(viteBaseConfig, viteProdConfig),//这种写法会将 viteProdConfig 对象的属性合并到 viteBaseConfig 对象中。它会修改 viteBaseConfig 对象，将 viteProdConfig 的属性添加到它身上，同时返回修改后的 viteBaseConfig 对象。
   // serve: () => Object.assign(viteBaseConfig, viteDevConfig),
   // build: () => Object.assign({}, viteBaseConfig, viteProdConfig), //新建一个空对象，然后将 viteBaseConfig 和 viteProdConfig 的属性合并到这个新对象中。
   // serve: () => Object.assign({}, viteBaseConfig, viteDevConfig),
-  "build": () =>{
-    console.log('生产环境');
-    return ({...viteBaseConfig,...viteProdConfig})
+  build: () => {
+    console.log('生产环境')
+    return { ...viteBaseConfig, ...viteProdConfig }
   },
-  "serve": () =>{
-    console.log('开发环境');
-    return ({...viteBaseConfig,...viteDevConfig})
-  } 
+  serve: () => {
+    console.log('开发环境')
+    return { ...viteBaseConfig, ...viteDevConfig }
+  },
 }
-export default defineConfig(({ command }) => {
+
+export default defineConfig(({ command, mode }) => {
   console.log('command', command)
+  console.log('mode', mode)
+  // process.cwd返回当前node进程的工作目录
+  const env = loadEnv(mode, process.cwd(), '') //第三个参数是.env名,一般是.env
+  console.log('env///', env)
+  // console.log(command)
   return envResolver[command]()
 })
 // vite解决了3个问题
